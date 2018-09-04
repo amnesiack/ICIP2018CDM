5185394422d32ae5a2dc1afeaf78ab209b999eab
amnesiack <amnesiackhit@hotmail.com>
2018-09-04 12:05:39 +0200

    Test

--- mosaicked_image_generation.m
+++ mosaicked_image_generation.m
@@ -1,6 +1,7 @@
 %Demo code for paper "COLOR IMAGE DEMOSAICKING USING A 3-STAGE CONVOLUTIONAL NEURAL NETWORK STRUCTURE"
 %K. Cui, Z. Jin, E. Steinbach, Color Image Demosaicking using a 3-stage Convolutional Neural Network Structure,IEEE International Conference on Image Processing (ICIP 2018), Athens, Greece, Oktober 2018.
 %Kai Cui <Kai.cui@tum.de>
+%Chair of Media Technology
 %Lehrstuhl fuer Medientechnik
 %Technische Universitaet Muenchen
 %Last modified 17.05.2018
